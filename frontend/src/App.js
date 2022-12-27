3415e5cfc6cf7ebd7de9064800141673ba4cf738
Michele Orrù <michele.orru@berkeley.edu>
2022-12-27 01:28:20 +0100

    Minor improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+// import also { useState } if willing to monitor changes to the whole page.
+import React from "react";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Radio, Form, Typography, Input, Select, Space } from "antd";
+import { AutoComplete, Button, Radio, Form, Typography, InputNumber, Select, Space, Layout, Input } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 
 const { Title } = Typography;
 const { Option } = Select;
 
 const libs = [
-  { label: "arkworks", value: "arkworks" },
+  { label: "arkworks", value: "arkworks", },
   { label: "blstrs", value: "blstrs"},
   { label: "dalek", value: "dalek", disabled: true}
 ];
@@ -17,20 +19,23 @@ const libs = [
 
 const estimates = {
   arkworks: [
-    {key: "msm", "value": "msm"},
-    {key: "add", "value": "add"},
+    {key: "msm", value: "msm"},
+    {key: "add", value: "add"},
   ],
   blstrs: [
-    {key: "msm", "value": "msm"},
-    {key: "add", "value": "add"},
+    {key: "msm", value: "msm"},
+    {key: "add", value: "add"},
   ]
 };
 
 function App() {
   const [form] = Form.useForm();
+  const [estimated_time, setTotal] = React.useState(0);
 
-  const onFinish = (values) => {
-    console.log("Received values of form:", values);
+  const onFormChange = (values) => {
+    const recipe = form.getFieldsValue();
+    console.log();
+    setTotal(estimated_time + 1);
   };
 
   const resetRecipe = () => {
@@ -39,11 +44,11 @@ function App() {
 
   return (
     <>
-      <Title>Zkalc</Title>
+      <Title align="center">zkalc</Title>
       <Form
+        style={{ padding: '0 50px', margin: '16x 0' }}
         form={form}
-        name="dynamic_form_complex"
-        onFinish={onFinish}
+        onChange={onFormChange}
         autoComplete="off"
       >
         <Form.Item
@@ -56,7 +61,8 @@ function App() {
           {(fields, { add, remove }) => (
             <>
               {fields.map((field) => (
-                <Space key={field.key} align="baseline">
+              <Space key={field.key} align="baseline">
+                <MinusCircleOutlined onClick={() => remove(field.name)} />
                   <Form.Item
                     noStyle
                     shouldUpdate={(prevValues, curValues) =>
@@ -67,35 +73,26 @@ function App() {
                     {() => (
                       <Form.Item
                         {...field}
-                        label="Operation"
                         name={[field.name, "op"]}
                         rules={[{ required: true, message: "Missing operation" }]}
                       >
                         <Select
                           disabled={!form.getFieldValue("lib")}
                           style={{ width: 230 }}
-                        >
-                          {(estimates[form.getFieldValue("lib")] || []).map(
-                            (item) => (
-                              <Option key={item["key"]} value={item["value"]}>
-                                {item["value"]}
-                              </Option>
-                            )
-                          )}
-                        </Select>
+                          bordered={false}
+                          showSearch
+                          options={estimates[form.getFieldValue("lib")]}
+                        />
                       </Form.Item>
                     )}
                   </Form.Item>
                   <Form.Item
                     {...field}
-                    label="Quantity"
                     name={[field.name, "quantity"]}
                     rules={[{ required: true, message: "Missing quantity" }]}
                   >
-                    <Input />
+                    <InputNumber min={1} max={100} style={{ width: 100 }}/>
                   </Form.Item>
-
-                  <MinusCircleOutlined onClick={() => remove(field.name)} />
                 </Space>
               ))}
 
@@ -103,22 +100,17 @@ function App() {
                 <Button
                   type="dashed"
                   onClick={() => add()}
-                  block
+                  size="large"
                   icon={<PlusOutlined />}
                 >
-                  Add sights
+                  Operation
                 </Button>
               </Form.Item>
             </>
           )}
         </Form.List>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
       </Form>
-
+      <p>Total time: {estimated_time} </p>
     </>
   );
 }
