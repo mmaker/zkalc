3454f4e26fdc48fc017f4043c592c29c7a0488c6
Michele Orrù <michele.orru@berkeley.edu>
2022-12-28 16:25:31 +0100

    L4t3x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,16 @@
 import "katex/dist/katex.min.css";
 
+
 // import also { useState } if willing to monitor changes to the whole page.
 import React from "react";
-import { Duration } from "luxon";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import {
-  AutoComplete,
-  Row, Col,
-  Button,
-  Radio,
-  Form,
-  Typography,
-  InputNumber,
-  Select,
-  Space,
-  Layout,
-  Input,
-} from "antd";
+import { Button, Radio, Form, Typography, Row, Col, Select, Space, Input, List } from "antd";
 import { Content } from "antd/es/layout/layout";
 import { InlineMath, BlockMath } from "react-katex";
+import {apply, applyTransformDependencies, parse} from "mathjs";
 
 const { Title } = Typography;
 const { Option } = Select;
-
 
 const libs = [
   { label: "arkworks", value: "arkworks", version: "XXX", url: "XXX" },
@@ -61,7 +49,7 @@ const operations = [
     value: "mul",
     description: "Multiscalar Multiplication",
   },
-]
+];
 // put this into another file, then do:
 // import estimates from "./estimates.json";
 
@@ -71,145 +59,153 @@ const estimates = {
       f: (n) => n * 2,
     },
     msm_g2: {
-      f: (n) => n*n*n,
+      f: (n) => n * n * n,
     },
     msm_ff: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
     msm_gt: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
   },
   blstrs: {
     msm_g1: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
     msm_g2: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
     msm_ff: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
     msm_gt: {
-      f: (n) => n*n,
+      f: (n) => n * n,
     },
   },
 };
 
-// objects are of the form
-// {
-//   "lib": "arkworks",
-//   "recipe": [
-//       {
-//           "op": "msm_ff",
-//           "quantity": 10
-//       },
-//       ...
-//   ]
-// }
-function estimatedTime(instructions) {
-  return instructions.recipe.map(item => {
-    var f = estimates[instructions.lib][item.op].f;
-    return f(item.quantity)
-  }).reduce((a, b) => a+b, 0)
-}
 
 function App() {
   const [form] = Form.useForm();
-  const [estimated_time, setTotal] = React.useState(0);
+  const [recipe, setRecipe] = React.useState([]);
+  const [lib, setLib] = React.useState("");
 
-  const onFormChange = (values) => {
-    const recipe = form.getFieldsValue();
-    console.log(recipe);
-    const estimated_time = estimatedTime(recipe);
-    setTotal(estimated_time);
-  };
+  const addIngredient = (ingredient) => {
+    const op = ingredient.op;
+    const formula = parse(ingredient.quantity);
+    const item = {op: op, quantity: formula};
+    setRecipe(recipe => [...recipe, item]);
+  }
+
+  const estimatedTime = (instructions) => {
+    return instructions.recipe
+      .map((item) => {
+        var f = estimates[instructions.lib][item.op].f;
+        return f(item.quantity.evaluate());
+      })
+      .reduce((a, b) => a + b, 0);
+  }
 
   const resetRecipe = () => {
     form.setFieldsValue({ recipe: [] });
   };
 
+  const removeIngredient = (index) => {
+    setRecipe(recipe.filter((_, i) => index !== i));
+  };
+
+  const renderFormula = (formula) => {
+    const evaluation = formula.evaluate();
+    if (evaluation.toString() === formula.toTex()) {
+        return formula.toTex();
+    } else {
+        return formula.toTex() + "=" + formula.evaluate();
+    }
+  }
+
   return (
     <>
-      <Title align="center">zkalc</Title>
+      <Title
+        align="center"
+        italic
+        fontSize={100}
+        letterSpacing={-3}
+      >
+        zkalc
+      </Title>
       <Form
+        align="center"
         style={{ padding: "0 50px", margin: "16x 0" }}
         form={form}
-        onChange={onFormChange}
+        onKeyUp={(event)=> {if (event.key === "Enter") form.submit()}}
+        onFinish={addIngredient}
         autoComplete="off"
       >
         <Form.Item
           name="lib"
-          label="Framework"
-          tooltip="Working on versions XXX"
           rules={[{ required: true, message: "Missing lib" }]}
         >
           <Radio.Group
-            onChange={resetRecipe}
+            onChange={(e) => setLib(e.target.value)}
             optionType="button"
             options={libs}
           />
         </Form.Item>
-        <Form.List name="recipe" wrapperCol={1}>
-          {(fields, { add, remove }) => (
-            <>
-              {fields.map((field) => (
-                <Space
-                  key={field.key}
-                  align="baseline"
-                  style={{ display: "flex", marginBottom: 8 }}
-                >
-                  <MinusCircleOutlined onClick={() => remove(field.name)} />
-                  <Form.Item
-                    noStyle
-                    shouldUpdate={(prevValues, curValues) =>
-                      prevValues.lib !== curValues.lib ||
-                      prevValues.recipe !== curValues.recipe
-                    }
-                  >
-                    {() => (
-                      <Form.Item
-                        {...field}
-                        name={[field.name, "op"]}
-                        rules={[
-                          { required: true, message: "Missing operation" },
-                        ]}
-                      >
-                        <Select
-                          style={{ width: 150 }}
-                          bordered={false}
-                          showSearch
-                          options={operations} // [form.getFieldValue("lib")]}
-                        />
-                      </Form.Item>
-                    )}
-                  </Form.Item>
-                  <Form.Item
-                    {...field}
-                    name={[field.name, "quantity"]}
-                    rules={[{ required: true, message: "Missing quantity" }]}
-                  >
-                    <InputNumber min={1} max={100} style={{ width: 100 }} />
-                  </Form.Item>
-                </Space>
-              ))}
-
-              <Form.Item wrapperCol={2}>
-                <Button
-                  type="dashed"
-                  disabled={!form.getFieldValue("lib")}
-                  onClick={() => add()}
-                  size="large"
-                  icon={<PlusOutlined />}
-                >
-                  Operation
-                </Button>
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
+        <Space align="baseline" >
+        <Form.Item
+          name="op"
+          rules={[{ required: true, message: "Missing operation" }]}
+        >
+          <Select
+            style={{ width: 150 }}
+            bordered={false}
+            placeholder="pk ops"
+            showSearch
+            options={operations}
+          />
+        </Form.Item>
+          <Form.Item
+            name="quantity"
+            rules={[{ required: true, message: "Missing quantity" }]}
+          >
+            <Input placeholder="2^64 + 100"  />
+          </Form.Item>
+          <Form.Item>
+          <Button
+            type="dashed"
+            disabled={!form.getFieldValue("lib")}
+            size="large"
+            icon={<PlusOutlined />}
+          >
+          </Button>
+        </Form.Item>
+        </Space>
       </Form>
-      <p>Total time: {Duration.fromMillis(estimated_time).rescale().toHuman({ unitDisplay: "short" })} </p>
+      <Row justify="center">
+        <Col align="center" span={8} offset={10}>
+        <Typography.Paragraph align="right"> Total time: {estimatedTime({lib: lib, recipe: recipe})} </Typography.Paragraph>
+        </Col>
+      </Row>
+      <Row justify="center">
+      <List
+        dataSource={recipe}
+        renderItem={(ingredient, index) => (
+          <List.Item key={index}>
+            <Col span={20} align="right">
+            <InlineMath>{renderFormula(ingredient.quantity)}</InlineMath>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            {ingredient.op}:
+            </Col>
+            <Col span={10}>
+            {estimatedTime({lib: lib, recipe: [ingredient]})}
+            </Col>
+            <Col span={5}>
+            <MinusCircleOutlined onClick={() => removeIngredient(index)}/>
+            </Col>
+          </List.Item>
+        )}
+      />
+      </Row>
     </>
   );
 }
