a37d2303f83886ba6634640b13519b8c65fb8293
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-07 15:48:41 +0000

    Add antd

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "antd/dist/reset.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app">
+      <Component {...pageProps} />
+    </div>
+  );
 }
