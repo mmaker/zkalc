8a5360eb614c47fd7ff6a92ae579042364661ce3
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-10 15:26:16 +0000

    Split recipe item

--- frontend/components/recipe.js
+++ frontend/components/recipe.js
@@ -4,40 +4,59 @@ import { formatNumber, formatFormula } from "../lib/formula";
 import { InlineMath } from "react-katex";
 import { MinusCircleOutlined } from "@ant-design/icons";
 
-export const Recipe = ({
-    recipe,
-    removeIngredient,
-    operations,
-    estimatedTime,
+const RecipeItem = ({
+  operations,
+  ingredient,
+  removeIngredient,
+  estimatedTime,
 }) => {
-  return <List
-    dataSource={recipe}
-    style={{ width: "90vh" }}
-    renderItem={(ingredient, index) => {
-      return (
-        <List.Item key={index}>
-          <Col span={14}>
-            <InlineMath math={formatFormula(ingredient.quantity)} />
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <Tooltip
-              placement="top"
-              color="#108ee9"
-              overlayInnerStyle={{
-                width: operations[ingredient.op].tooltip_width,
-              }}
-              title={operations[ingredient.op].tooltip}
-            >
-              {operations[ingredient.op].description}
-            </Tooltip>
-          </Col>
-          <Col span={6} align="right">
-            {estimatedTime([ingredient])}
-          </Col>
-          <Col span={1}>
-            <MinusCircleOutlined onClick={() => removeIngredient(index)} />
-          </Col>
-        </List.Item>
-      );
-    }}
-  />;
+  return (
+    <List.Item>
+      <Col span={14}>
+        <InlineMath math={formatFormula(ingredient.quantity)} />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Tooltip
+          placement="top"
+          color="#108ee9"
+          overlayInnerStyle={{
+            width: operations[ingredient.op].tooltip_width,
+          }}
+          title={operations[ingredient.op].tooltip}
+        >
+          {operations[ingredient.op].description}
+        </Tooltip>
+      </Col>
+      <Col span={6} align="right">
+        {estimatedTime([ingredient])}
+      </Col>
+      <Col span={1}>
+        <MinusCircleOutlined onClick={removeIngredient} />
+      </Col>
+    </List.Item>
+  );
+};
+
+export const Recipe = ({
+  recipe,
+  removeIngredient,
+  operations,
+  estimatedTime,
+}) => {
+  return (
+    <List
+      dataSource={recipe}
+      style={{ width: "90vh" }}
+      renderItem={(ingredient, index) => {
+        return (
+          <RecipeItem
+            key={index}
+            ingredient={ingredient}
+            operations={operations}
+            estimatedTime={estimatedTime}
+            removeIngredient={() => removeIngredient(index)}
+          />
+        );
+      }}
+    />
+  );
 };
