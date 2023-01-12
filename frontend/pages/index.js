befa6ade08c67e70dd6fa17893163b17faade471
Michele Orrù <michele.orru@berkeley.edu>
2023-01-12 13:10:16 +0100

    Fix path for estimates

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -49,10 +49,10 @@ import libraries from "../data/libraries.json";
 import machines from "../data/machines.json";
 
 // Import our benchmark results
-import estBls12381ArkM1 from "../public/bls12-381/arkworks/m1pro.json";
-import estBls12381ArkT450 from "../public/bls12-381/arkworks/t450.json";
-import estBls12381BlstM1 from "../public/bls12-381/blstrs/m1pro.json";
-import estBls12381BlstT450 from "../public/bls12-381/blstrs/t450.json";
+import estBls12381ArkM1 from "../data/bls12-381/arkworks/m1pro.json";
+import estBls12381ArkT450 from "../data/bls12-381/arkworks/t450.json";
+import estBls12381BlstM1 from "../data/bls12-381/blstrs/m1pro.json";
+import estBls12381BlstT450 from "../data/bls12-381/blstrs/t450.json";
 
 const estimates = {
   blstrs: {
