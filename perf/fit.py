40cf9178f8293efaf363d4e407ce47b4f788ac2b
Michele Orrù <michele.orru@berkeley.edu>
2023-01-24 10:14:30 +0100

    Fix fit.py parser.

--- perf/fit.py
+++ perf/fit.py
@@ -46,7 +46,7 @@ op_ids = [
 probes = {
     # zkalc naming convention
     r'.*/msm/G([12t])/(\d+)': lambda x, y: (f"msm_{x}", int(y)),
-    f'({"|".join(op_ids.keys())})': lambda x: (x, 1),
+    f'({"|".join(op_ids)})': lambda x: (x, 1),
 
     # compatibility with arkworks ark-bench naming
     f'Arithmetic for .*::(G[12])/({"|".join(ark_names.keys())})': lambda x, y: (f"{ark_names[y]}_{x}", 1),
