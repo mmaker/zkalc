8420f470f9ac86908c193239ab73d7dfc19536b9
George Kadianakis <desnacked@riseup.net>
2023-02-01 23:49:33 +0200

    Attempt to add vercel analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,14 +3,14 @@ import "antd/dist/reset.css";
 import "highlight.js/styles/github.css";
 
 import NextNProgress from 'nextjs-progressbar';
-
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NextNProgress />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
