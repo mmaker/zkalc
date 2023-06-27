b7bc7e5c11a3281c37de1c566d0c0a60d4250b42
Michele Orrù <michele.orru@berkeley.edu>
2023-06-28 00:18:35 +0200

    Review of halo2_curves

--- backend/halo2_curves/benches/bench_halo2_curves.rs
+++ backend/halo2_curves/benches/bench_halo2_curves.rs
@@ -4,60 +4,51 @@ use std::any::type_name;
 
 use criterion::measurement::Measurement;
 use criterion::{BenchmarkGroup, BenchmarkId};
-use ff::{PrimeField, Field};
-use pairing::Engine;
+use ff::{Field, PrimeField};
 use halo2curves::{bn256, pairing, CurveExt};
+use pairing::Engine;
 use rand_core::OsRng;
 
-// Benchmark Addition in Scalar Field
 fn bench_add_ff<F: PrimeField, M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
-    let a: F = F::random(OsRng);
-    let b: F = F::random(OsRng); 
-    
-    let mut lhs = black_box(a);
-    let rhs = black_box(&b);
     c.bench_function("add_ff", |b| {
-        b.iter(|| {
-            F::add_assign(&mut lhs, rhs)
-        });
+        let lhs = F::random(OsRng);
+        let rhs = F::random(OsRng);
+        b.iter(|| black_box(lhs) + black_box(rhs));
     });
 }
 
-// Benchmark Multiplication in Scalar FIeld
 fn bench_mul_ff<F: PrimeField, M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
-    let a: F = F::random(OsRng);
-    let b: F = F::random(OsRng); 
-    let mut lhs = black_box(a);
-    let rhs = black_box(&b);
     c.bench_function("mul_ff", |b| {
-        b.iter(|| {
-            F::mul_assign(&mut lhs, rhs)
-        });
+        let lhs = F::random(OsRng);
+        let rhs = F::random(OsRng);
+        b.iter(|| black_box(lhs) * black_box(rhs));
     });
 }
 
-// Benchmark Addition in Elliptic Curve Group
 fn bench_add_ec<G: CurveExt, M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
-    let lhs = G::random(OsRng);
-    let rhs = G::random(OsRng);
-    let mut lhs = black_box(lhs);
-    let rhs = black_box(&rhs);
-    let group_name = type_name::<G>();
-    c.bench_function(BenchmarkId::new(group_name, "add"), |b| {
-        b.iter(|| {
-            lhs.add_assign(rhs);
-        });
+    let group_name = type_name::<G>().split("::").last().unwrap();
+    let op_name = format!("add_{}", group_name);
+    c.bench_function(op_name.as_str(), |b| {
+        let lhs = G::random(OsRng);
+        let rhs = G::random(OsRng);
+        b.iter(|| lhs.add(rhs));
     });
 }
 
-// TODO - Multiplication in EC
+fn bench_mul_ec<G: CurveExt, M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
+    let group_name = type_name::<G>().split("::").last().unwrap();
+    let op_name = format!("mul_{}", group_name);
+    c.bench_function(op_name.as_str(), |b| {
+        let lhs = G::random(OsRng);
+        let rhs = G::ScalarExt::random(OsRng);
+        b.iter(|| black_box(lhs) * black_box(rhs));
+    });
+}
 
-// Sampling Scalars for MSM
-fn get_data(n: usize) -> (Vec<bn256::G1Affine>, Vec<bn256::Fr>) {
-    const MAX_N: usize = 1 << 22;
-    assert!(n <= MAX_N);
-
-    let (points, scalars): (Vec<_>, Vec<_>) = (0..n)
+// The MSM algorithm of halo2_curve can be found here: https://github.com/privacy-scaling-explorations/halo2curves/pull/29
+fn bench_msm(group: &mut BenchmarkGroup<'_, criterion::measurement::WallTime>) {
+    const MAX_SIZE: usize = 21;
+    let (points, scalars): (Vec<_>, Vec<_>) = (0..1<<MAX_SIZE)
         .map(|_| {
             let point = bn256::G1Affine::random(OsRng);
             let scalar = bn256::Fr::random(OsRng);
@@ -65,17 +56,7 @@ fn get_data(n: usize) -> (Vec<bn256::G1Affine>, Vec<bn256::Fr>) {
         })
         .unzip();
 
-    (points, scalars)
-}
-
-
-// The MSM algorithm of halo2_curve can be found here: https://github.com/privacy-scaling-explorations/halo2curves/pull/29
-fn bench_msm(group: &mut BenchmarkGroup<'_, criterion::measurement::WallTime>) {
-    
-    const MAX_SIZE: usize = 20;
-    let (points, scalars) = get_data(1 << MAX_SIZE);
-
-    for logsize in 1..=MAX_SIZE {
+    for logsize in 11..=MAX_SIZE {
         // Dynamically control sample size so that big MSMs don't bench eternally
         if logsize > 20 {
             group.sample_size(10);
@@ -95,26 +76,25 @@ fn bench_msm(group: &mut BenchmarkGroup<'_, criterion::measurement::WallTime>) {
     }
 }
 
-// Benchmark pairing
-fn bench_pairing< M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
-    let g1 = bn256::G1::generator();
-    let g2 = bn256::G2::generator();
+fn bench_pairing<M: Measurement>(c: &mut BenchmarkGroup<'_, M>) {
     c.bench_function("pairing", |r| {
-        r.iter(|| bn256::Bn256::pairing(&bn256::G1Affine::from(g1), &bn256::G2Affine::from(g2)))
+        let lhs = bn256::G1Affine::random(OsRng);
+        let rhs = bn256::G2Affine::random(OsRng);
+        r.iter(|| bn256::Bn256::pairing(&lhs, &rhs))
     });
 }
 
 fn bench_bn256(c: &mut Criterion) {
     let mut group = c.benchmark_group("bn256");
     bench_add_ff::<bn256::Fr, _>(&mut group);
-    bench_mul_ff::<bn256::Fr,_>(&mut group);
+    bench_mul_ff::<bn256::Fr, _>(&mut group);
     bench_add_ec::<bn256::G1, _>(&mut group);
+    bench_add_ec::<bn256::G2, _>(&mut group);
+    bench_mul_ec::<bn256::G1, _>(&mut group);
+    bench_mul_ec::<bn256::G2, _>(&mut group);
     bench_msm(&mut group);
     bench_pairing::<_>(&mut group);
 }
 
-criterion_group!(
-    benches,
-    bench_bn256
-);
+criterion_group!(benches, bench_bn256);
 criterion_main!(benches);
