9170cbec22a2d4b7ab9990ac35accabfb0fef4de
Stefanos Chaliasos <stefanos@chaliasos.com>
2023-07-26 14:19:07 +0300

    Fix ffjavascript issues
    * Do not rename the curve because pairing did not work
    * Fix async issues for msm

--- backend/ffjavascript/bench.js
+++ backend/ffjavascript/bench.js
@@ -5,11 +5,8 @@ import { BigBuffer, buildBn128, buildBls12381, F1Field } from "ffjavascript";
 import { Bench } from "tinybench";
 
 const curves = [
-  buildBls12381(false).then((curve) => {
-    curve.name = "bls12_381";
-    return curve;
-  }),
   buildBn128(false),
+  buildBls12381(false),
 ];
 
 function bench_add_ff(F) {
@@ -49,44 +46,42 @@ function bench_pairing(curve) {
   const g1 = curve.G1.timesScalar(curve.G1.g, x);
   const g2 = curve.G2.timesScalar(curve.G2.g, y);
   return () => {
-    const pre1 = curve.prepareG1(g1);
-    const pre2 = curve.prepareG2(g2);
-    const r1 = curve.millerLoop(pre1, pre2);
-    const r2 = curve.finalExponentiation(r1);
+    curve.pairing(g1, g2);
+    // Alternative way of performing the pairing operation
+    // the performance is pretty much the same
+    //const pre1 = curve.prepareG1(g1);
+    //const pre2 = curve.prepareG2(g2);
+    //const r1 = curve.millerLoop(pre1, pre2);
+    //const r2 = curve.finalExponentiation(r1);
   };
 }
 
-function bench_group(bench, name, fn, range) {
+function bench_fft(bench, name, F, range) {
   for (var i = range[0]; i < range[1]; i++) {
-    bench.add(name + "/" + i, fn(i));
-  }
-}
-
-function bench_fft(F) {
-  return (_n) => {
-    const n = Math.pow(2, _n);
+    const n = Math.pow(2, i);
     const x = new BigBuffer(n * F.n8);
     for (let i = 0; i < n; i++) {
       x.set(F.random(), i * F.n8);
     }
-    return () => {
-      F.fft(x);
-    };
-  };
+    bench.add(name + "/" + n, async () => {
+      await F.fft(x)
+    });
+  }
 }
 
-function bench_msm(G, Fr) {
-  return async (_n) => {
-    // size is log-scale
-    let n = Math.pow(2, _n);
-    const scalars = new BigBuffer(n * Fr.n8);
-    const bases = new BigBuffer(n * G.F.n8 * 2);
-    for (let i = 0; i < n; i++) {
-      scalars.set(Fr.random(), i * Fr.n8);
-      bases.set(G.toAffine(G.timesFr(G.g, Fr.random())), i * G.F.n8 * 2);
+function bench_msm(bench, name, G, Fr, range) {
+    for (var i = range[0]; i < range[1]; i++) {
+      let n = Math.pow(2, i);
+      const scalars = new BigBuffer(n*Fr.n8);
+      const bases = new BigBuffer(n*G.F.n8*2);
+      for (let i = 0; i < n; i++) {
+        scalars.set(Fr.random(), i * Fr.n8);
+        bases.set(G.toAffine(G.timesFr(G.g, Fr.random())), i * G.F.n8 * 2);
+      }
+      bench.add(name + "/" + n, async () => {
+        await G.multiExpAffine(bases, scalars, false, "");
+      });
     }
-    await G.multiExpAffine(bases, scalars, false, "");
-  };
 }
 
 async function run() {
@@ -94,23 +89,29 @@ async function run() {
 
   for (const curve of curves) {
     const c = await curve;
+    // If we do not do it like that, then the pairing operation does not work
+    // properly.
+    let name = c.name;
+    if (name == "bls12381") {
+        name = "bls12_381";
+    }
 
     bench
-      .add(c.name + "/add_ff", bench_add_ff(c.Fr))
-      .add(c.name + "/invert", bench_invert(c.Fr))
-      .add(c.name + "/mul_G1", bench_mul_ec(c.G1, c.Fr))
-      .add(c.name + "/mul_G2", bench_mul_ec(c.G2, c.Fr))
-      .add(c.name + "/pairing", bench_pairing(c))
-      .add(c.name + "/add_G1", bench_add_ec(c.G1, c.Fr))
-      .add(c.name + "/add_G2", bench_add_ec(c.G2, c.Fr));
+      .add(name + "/add_ff", bench_add_ff(c.Fr))
+      .add(name + "/invert", bench_invert(c.Fr))
+      .add(name + "/mul_G1", bench_mul_ec(c.G1, c.Fr))
+      .add(name + "/mul_G2", bench_mul_ec(c.G2, c.Fr))
+      .add(name + "/pairing", bench_pairing(c))
+      .add(name + "/add_G1", bench_add_ec(c.G1, c.Fr))
+      .add(name + "/add_G2", bench_add_ec(c.G2, c.Fr));
 
-    bench_group(bench, c.name + "/msm_G1", bench_msm(c.G1, c.Fr), [10, 21]);
-    bench_group(bench, c.name + "/msm_G2", bench_msm(c.G2, c.Fr), [10, 21]);
-    bench_group(bench, c.name + "/fft", bench_fft(c.Fr), [10, 21]);
+    bench_msm(bench, name + "/msm_G1", c.G1, c.Fr, [1, 15]);
+    bench_msm(bench, name + "/msm_G2", c.G2, c.Fr, [1, 15]);
+    bench_fft(bench, name + "/fft", c.Fr, [1, 15]);
   }
 
   await bench.run();
-  // console.table(bench.table());
+  //console.table(bench.table());
   process.stdout.write(JSON.stringify(bench.table()));
   return bench.results;
 }
