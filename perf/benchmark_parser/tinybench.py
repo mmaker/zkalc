e000fd954f105b77e91f62687dcf2acb0948cde4
Michele Orrù <michele.orru@berkeley.edu>
2023-08-08 01:55:27 +0200

    Fix frontend data

--- perf/benchmark_parser/tinybench.py
+++ perf/benchmark_parser/tinybench.py
@@ -45,18 +45,20 @@ probes = {
     r'msm/(G[12t]|ff)/(\d+)': lambda x, y: (f"msm_{x}", int(y)),
 }
 
-def extract_measurements(bench_output):
+def extract_measurements(bench_output, curve = None):
     measurements = defaultdict(lambda: defaultdict(list))
 
     # Parse benchmarks and make them ready for fitting
     for measurement in itertools.chain(*bench_output):
         # DEPRECATED: Extra data from json if in table format
         if "Average Time (ns)" in measurement:
+            if curve is not None and curve not in measurement["Task Name"]: continue
             operation, size = parse_benchmark_description(measurement["Task Name"], probes)
             measurement_in_ns = measurement["Average Time (ns)"]
             measurements[operation]["range"].append(size)
             measurements[operation]["results"].append(measurement_in_ns)
         else: # if "mean" in measurement:
+            if curve is not None and curve not in measurement["name"]: continue
             to_ns = 1000 * 1000
             operation, size = parse_benchmark_description(measurement["name"], probes)
             measurement_in_ns = to_ns * measurement["mean"]
@@ -68,17 +70,13 @@ def extract_measurements(bench_output):
 
 
 def main(ins=[sys.stdin], outs=sys.stdout, curve=None):
-    bench_output = [json.loads(line) for i in ins for line in i if line.strip() and (curve is None or curve.lower() in line.lower())]
-    if not bench_output:
+    bench_output = [json.loads(line) for i in ins for line in i if line.strip()]
+    results = extract_measurements(bench_output, curve)
+
+
+    if not results:
         # outs.write("{}")
         return False
-    # Dictionary of results in format: { operation : measurements }
-    results = {}
-    # Extract measurements into a nested dictionary: { operation : {size : time_in_microseconds }}
-    measurements = extract_measurements(bench_output)
-    # Re-format the measurements into a dictionary: {operation : {range: [sizes], results: [times]}
-    results = {operation: export_measurement(
-        measurements[operation]) for operation in measurements}
 
     # Encode the functions as a JSON object
     json_data = json.dumps(results, indent=2)
