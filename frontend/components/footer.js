ff5f14e5359e63f391f8e7ae3b2cc197f6e43b86
George Kadianakis <desnacked@riseup.net>
2024-02-26 15:07:55 +0200

    Restore authors
    This reverts commit 8b60085300501e098c3ccf19bdafd60fb9179296.

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Typography } from "antd";
+import { authors } from "../../AUTHORS";
 
 const { Text } = Typography;
 
 const Authors = () => {
   return (
     <Text style={{ fontSize: 13 }}>
-        
+      Developed by
+      <a href={authors[0].website}> {authors[0].name}</a> and{" "}
+      <a href={authors[1].website}>{authors[1].name}</a>.
     </Text>
   );
 };
